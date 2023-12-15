0281ea2e0da091d95c65c595ea838215748f2e6b
roronan13 <ronan.pothier@gmail.com>
2023-12-15 16:14:58 +0100

    15 dec

--- ft_printf.c
+++ ft_printf.c
@@ -6,15 +6,37 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 00:16:30 by rpothier          #+#    #+#             */
-/*   Updated: 2023/12/15 14:09:32 by rpothier         ###   ########.fr       */
+/*   Updated: 2023/12/15 15:37:58 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "stdio.h"
 
+int cestquelpourcentbebe()
+{
+	cest un c?
+		appel la fonction qui gere c
+}
+
+int	parcourir la chaine(va_list, const char*)
+{
+	while (chaine de caractere)
+		if (pourcent+ apres cest un% s, d, i..)
+			on va dans la fonction qui gere chaque pourcent
+		else
+			on ecrit
+	return(nombre de caratceres imprimes)
+}
+
 /* int	ft_printf(const char *object, ...)
 {
+	va_list arguments
+	int nbr
+	va_start()
+	nbr = parcourir la chaine
+	va_end
+	return nbr
 	
 } */
 
