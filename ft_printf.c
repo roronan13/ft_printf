9fb926f114078ebe6283cf612f70fdf7cb0b0814
roronan13 <ronan.pothier@gmail.com>
2024-01-05 16:32:01 +0100

    5 jan

--- ft_printf.c
+++ ft_printf.c
@@ -6,20 +6,20 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 00:16:30 by rpothier          #+#    #+#             */
-/*   Updated: 2024/01/05 14:50:48 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/01/05 16:15:15 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "stdio.h"
 
-int cestquelpourcentbebe()
+/* int cestquelpourcentbebe()
 {
 	cest un c?
 		appel la fonction qui gere c
-}
+} */
 
-int	parcourir la chaine(va_list, const char*)
+/* int	parcourir la chaine(va_list, const char*)
 {
 	while (chaine de caractere)
 		if (pourcent+ apres cest un% s, d, i..)
@@ -27,7 +27,7 @@ int	parcourir la chaine(va_list, const char*)
 		else
 			on ecrit
 	return(nombre de caratceres imprimes)
-}
+} */
 
 /* int	ft_printf(const char *object, ...)
 {
@@ -40,18 +40,18 @@ int	parcourir la chaine(va_list, const char*)
 	
 } */
 
-static	ft_sign(object, a)
+/* static	ft_sign(object, a)
 {
 	if (a == 'c')
 	{
 		
 	}
-}
+} */
 
 int	ft_printf(const char *object, ...)
 {
-	unsigned long int	i;
-	char				a;
+	int		i;
+	char	a;
 
 	i = 0;
 	while (object[i])
