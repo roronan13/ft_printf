f8da85f7794cbd9ae4ec650669ae2b52969c8ae9
roronan13 <ronan.pothier@gmail.com>
2024-01-12 15:49:10 +0100

    12 jan

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 00:16:30 by rpothier          #+#    #+#             */
-/*   Updated: 2024/01/12 12:08:59 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/01/12 14:54:08 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "stdio.h"
-#include "stdlib.h"
+//#include "stdlib.h"
 
 /* int	parcourir la chaine(va_list, const char*)
 {
@@ -35,9 +35,9 @@
 	
 } */
 
-/* static void	ft_check_sign(char a, unsigned int *nbr, int *i, va_list ap)
+static void	ft_check_sign(char a, int *nbr, int *i, va_list ap)
 {
-	//unsigned int	nbr;k
+	//unsigned int	nbr;
 	
 	if (a == 'c' || a == 's' || a == 'p' || a == 'd' || a == 'i'
 		|| a == 'u' || a == 'x' || a == 'X' || a == '%')
@@ -45,9 +45,9 @@
 			nbr = ft_sign(a, nbr, ap);
 			i++;
 		}
-} */
+}
 
-/* static int	ft_sign(char a, unsigned int nbr, va_list ap)
+static int	ft_sign(char a, int nbr, va_list ap)
 {
 	if (a == 'c')
 		nbr += ft_printf_c(va_arg(ap, char));
@@ -62,13 +62,13 @@
 	if (a == 'u')
 		nbr += ft_printf_u(va_arg(ap, unsigned int));
 	return (nbr);
-} */
+}
 
-/* int	ft_printf(const char *object, ...)
+int	ft_printf(const char *object, ...)
 {
 	unsigned int	i;
 	char			a;
-	unsigned int	nbr;
+	int				nbr;
 
 	va_list(ap);
 	va_start(ap, object);
@@ -90,7 +90,7 @@
 	}
 	va_end(ap);
 	return (nbr);
-} */
+}
 
 int	main(void)
 {
@@ -99,5 +99,6 @@ int	main(void)
 	ptr = malloc(sizeof(int) * 100);
 	printf("%c\n%d\na\n%c\n", 'a', 1, 'b');
 	printf("%p\n", ptr);
+	printf("%p\n", &ptr);
 	return (0);
 }
