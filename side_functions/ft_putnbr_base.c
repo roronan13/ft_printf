ea886e3694fc00a302f4eefd5f30e75a4884a2bb
roronan13 <ronan.pothier@gmail.com>
2024-01-17 17:46:38 +0100

    17 jan

--- side_functions/ft_putnbr_base.c
+++ side_functions/ft_putnbr_base.c
@@ -1,18 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_x.c                                      :+:      :+:    :+:   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/17 15:19:29 by rpothier          #+#    #+#             */
-/*   Updated: 2024/01/17 16:35:23 by rpothier         ###   ########.fr       */
+/*   Created: 2024/01/17 16:34:09 by rpothier          #+#    #+#             */
+/*   Updated: 2024/01/17 17:45:26 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/ft_printf.h"
+#include "../includes/ft_printf.h"
 
-int	ft_printf_x(int n)
+int	ft_putnbr_base(int n, char *base)
 {
-	
+	int	size_base;
+
+	size_base = ft_strlen(base);
+	if (n == -2147483648)
+	{
+		write(1, "-80000000", 9);
+		return (9);
+	}
+	else if (n < 0)
+	{
+		n *= -1;
+		write(1, '-', 1);
+	}
+	if ()
 }
