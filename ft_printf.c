0ef51146f0aba6e297ee1c9e8c79ae76336203d2
roronan13 <ronan.pothier@gmail.com>
2024-01-24 14:36:38 +0100

    24 jan

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rpothier <rpothier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 00:16:30 by rpothier          #+#    #+#             */
-/*   Updated: 2024/01/24 14:20:57 by rpothier         ###   ########.fr       */
+/*   Updated: 2024/01/24 14:35:26 by rpothier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,19 +19,19 @@ static int	ft_sign(char a, va_list ap)
 	local_nbr = 0;
 	if (a == 'c')
 		local_nbr = ft_printf_c(va_arg(ap, int));
-	if (a == 's')
+	else if (a == 's')
 		local_nbr = ft_printf_s(va_arg(ap, char *));
-	if (a == 'p')
+	else if (a == 'p')
 		local_nbr = ft_printf_p(va_arg(ap, void *));
-	if (a == 'd' || a == 'i')
+	else if (a == 'd' || a == 'i')
 		local_nbr = ft_printf_d_i(va_arg(ap, int));
-	if (a == 'u')
+	else if (a == 'u')
 		local_nbr = ft_printf_u(va_arg(ap, unsigned int));
-	if (a == 'x')
+	else if (a == 'x')
 		local_nbr = ft_printf_x_low(va_arg(ap, unsigned int));
-	if (a == 'X')
+	else if (a == 'X')
 		local_nbr = ft_printf_x_up(va_arg(ap, unsigned int));
-	if (a == '%')
+	else if (a == '%')
 		local_nbr = ft_printf_percent();
 	else
 	{
@@ -69,65 +69,3 @@ int	ft_printf(const char *format, ...)
 	va_end(ap);
 	return (global_nbr);
 }
-
-//int	main(void)
-//{
-/*	char	c;
-	int		a;
-	int		b;
-	int		d;
-	int		y;
-	char	*s;
-	void	*p;
-	int 	x; */
-	
-	//ptr = malloc(sizeof(int) * 100);
-	//d = 155555;
-	/*c = 122; //z
-	a = printf("printf: %d\n", printf("oui %c\n", c)); //checker longueur vraie printf
-	b = printf("ronanf: %d\n", ft_printf("non %c\n", c)); //checker longueur ft_printf
-	ft_printf("petit test %c\n", c);
-	printf("petit test %c\n", c);
-	printf("len printf: %d\nlen ronan %d\n", a, b);*/
-	/* x = printf("%i\n", d);
-	y = ft_printf("%i\n", d);
-	printf("%i\n", d);
-	ft_printf("%i\n", d);
-	printf("%i\n", x);
-	printf("%i\n", y); */
-	//s = "\x09";
-	/* s = "Allez";
-	x = printf("i/ Eh-%s-allez\n", s);
-	y = ft_printf("l/ Eh-%s-allez\n", s);
-	printf("%s\n", s);
-	ft_printf("roro %s\n", s);
-	printf("%d\n", x);
-	printf("riri %d\n", y); */
-	/* printf("vraie : %p\n", p);
-	ft_printf("moi : %p\n", p); */
-	/*x = 1000;
-	printf("vraie 1 : %X\n", x);
-	ft_printf("moi 1 : %X\n", x);
-	printf("%d\n", printf("vraie : %X\n", x));
-	printf("%d\n", ft_printf("moi : %X\n", x));*/
-/* 	printf("vraie 1 : %d\n", -50);
-	ft_printf("moi 1 : %d\n", -50);
-	printf("%d\n", printf("vraie : %d\n", -50));
-	printf("%d\n", ft_printf("moi : %d\n", -50));
-	return (0);*/
-/* 	char	*ptr;
-	int a;
-	int b;
-	
-	a = printf("%d\n", -1);
-	b = ft_printf("%d\n", -1);
-	printf("%d\n", a);
-	printf("%d\n", b); */
-	
-/* 	int	a;
-	int b;
-	a = printf("%z\n", 10);
-	b = ft_printf("%z\n", 10);
-	printf("%d\n", a);
-	printf("%d\n", b);*/
-//}
